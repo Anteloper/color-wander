b345aa6e7ae3517c9644be38991c95886677ee7e
Oliver Hill <obh6@georgetown.edu>
2021-10-17 21:08:11 -0500

    asd

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const createConfig = require('./config');
 const createFileRenderer = require('./lib/createFileRenderer');
 
-module.exports.generateArt = async function generateArt(seed) {
+const generateArt = async function generateArt(seed) {
 	const config = createConfig(seed);
 	return await createFileRenderer(config);
+}
+
+module.exports = {
+	generateArt
 }
